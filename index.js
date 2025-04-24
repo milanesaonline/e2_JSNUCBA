2fe49c03f4542eff0fe484850b079cca9fe80081
milanesaonline <milanesa.dg@gmail.com>
2025-04-23 21:09:27 -0300

    ejercicio listo

--- index.js
+++ index.js
@@ -42,8 +42,52 @@ const pizzas = [
 
   {
     id: 6,
-    nombre: "pizza con Anana",
+    nombre: "pizza con Ananá",
     precio: 600,
-    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    ingredientes: ["Muzzarella", "Tomate", "Ananá"],
   },
 ];
+
+
+////////////////////////////////////////
+
+// a) Las pizzas que tengan un id impar
+console.log("\n 🍕 a) Las pizzas con ID impar son:");
+pizzas.forEach(pizza => {
+  if (pizza.id % 2 !== 0) {
+    console.log(`- La ${pizza.nombre} tiene un ID impar (${pizza.id})`);
+  }
+});
+
+///////////////////////////////////////
+
+// b) ¿Hay alguna pizza que valga menos de $600? 
+console.log("\n 🍕 b) ¿Hay alguna pizza que salga menos de $600?");
+const hayPizzaBarata = pizzas.some(pizza => pizza.precio < 600);
+
+if (hayPizzaBarata) {
+  console.log("Siii, tenemos pizza a menos de $600 🙌🏻");
+} else {
+  console.log("No, todas las pizzas salen más de $600 👎🏻");
+}
+
+///////////////////////////////////////
+
+// c) El nombre de cada pizza con su respectivo precio
+console.log("\n 🍕 c) Lista de precios de nuestras pizzas:");
+pizzas.forEach(pizza => {
+  console.log(`- La ${pizza.nombre} sale $${pizza.precio}`);
+});
+
+///////////////////////////////////////
+
+// d) Todos los ingredientes de cada pizza
+console.log("\n 🍕 d) Lista de ingredientes:");
+pizzas.forEach(pizza => {
+  console.log(`- Los ingredientes que usamos para la ${pizza.nombre} son:`);
+  pizza.ingredientes.forEach(ingrediente => {
+    console.log(`   • ${ingrediente}`);
+  });
+});
+
+//////////////////////////////////////
